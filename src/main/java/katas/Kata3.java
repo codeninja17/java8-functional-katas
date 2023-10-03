28fe869213041ec3502feb63f36244a5e5c03d72
codeninja17 <ankitkr09@gmail.com>
2023-10-03 20:04:34 +0530

    solved kata 4

--- src/main/java/katas/Kata3.java
+++ src/main/java/katas/Kata3.java
@@ -5,7 +5,10 @@ import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Use map() and flatMap() to project and flatten the movieLists into an array of video ids (flatMap(c -> c.stream()))
@@ -16,6 +19,9 @@ public class Kata3 {
     public static List<Integer> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(1, 2, 3);
+        return movieLists
+                .stream()
+                .flatMap(movieList -> movieList.getVideos().stream())
+                .map(movie -> movie.getId()).collect(Collectors.toList());
     }
 }
--- src/main/java/katas/Kata4.java
+++ src/main/java/katas/Kata4.java
@@ -3,11 +3,13 @@ package katas;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import model.BoxArt;
+import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve id, title, and a 150x200 box art url for every video
@@ -18,6 +20,13 @@ public class Kata4 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        return movieLists.stream()
+                .flatMap(movieList -> movieList.getVideos().stream())
+                .map(Kata4::movieMapper)
+                .collect(Collectors.toList());
+    }
+
+    public static Map movieMapper(Movie movie) {
+        return ImmutableMap.of("id", movie.getId(), "title", movie.getTitle(), "boxart", new BoxArt(150, 200, movie.getUri()));
     }
 }
